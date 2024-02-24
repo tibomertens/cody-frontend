2736cc062576f28ac1a837cb5a7a4c1142423fd7
QuinttAdam <116001984+QuinttAdam@users.noreply.github.com>
2024-02-24 22:07:33 +0100

    feat: add fully functioning sidenav

--- src/components/SideNav.vue
+++ src/components/SideNav.vue
@@ -1,43 +1,149 @@
 <script setup>
-  document.addEventListener('DOMContentLoaded', () => {
-    //select the div with dropdown class
-    const dropdown = document.querySelector('.dropdown');
-    const hamburger = document.querySelector('.hamburger');
-    //add event listener to the hamburger icon
-    hamburger.addEventListener('click', () => {
-      //toggle the hidden class
-      dropdown.classList.toggle('hidden');
-      //change fa-solid fa-bars to fa-solid fa-xmark
-      hamburger.classList.toggle('fa-bars');
-      hamburger.classList.toggle('fa-xmark');
-    });
+import { ref, onMounted, watch } from "vue";
+import { useRoute } from "vue-router";
+
+document.addEventListener("DOMContentLoaded", () => {
+  //select the div with dropdown class
+  const dropdown = document.querySelector(".dropdown");
+  const hamburger = document.querySelector(".hamburger");
+  //add event listener to the hamburger icon
+  hamburger.addEventListener("click", () => {
+    //toggle the hidden class
+    dropdown.classList.toggle("hidden");
+    //change fa-solid fa-bars to fa-solid fa-xmark
+    hamburger.classList.toggle("fa-bars");
+    hamburger.classList.toggle("fa-xmark");
   });
+});
+
+const currentRoute = ref("");
+const route = useRoute();
+
+onMounted(() => {
+  currentRoute.value = route.path;
+
+  // Watch for route changes
+  watch(
+    () => route.path,
+    (newRoute) => {
+      currentRoute.value = newRoute;
+    }
+  );
+});
 </script>
 
-    
 <template>
   <div class="">
-    <div class="w-full h-15 bg-red-200 flex justify-between p-5">
+    <div class="w-full h-15 bg-[#EDF0F5] xl:w-[20%] xl:h-screen">
       <!-- Quintt: dit is voorbeeld, voeg eigen routes toe. -->
-      <img src="" alt="logo icon" />
-      <i class="hamburger fa-solid fa-bars self-center"></i>
+      <div class="flex justify-between p-5">
+        <img src="" alt="logo icon" />
+        <i class="hamburger fa-solid fa-bars self-center xl:hidden"></i>
+      </div>
+
+      <div class="hidden xl:block">
+        <div
+          class="pl-5 ml-5 py-[12px] rounded-l-[5px]"
+          :class="{ 'bg-[#9EBDFF]': currentRoute === '/home' }"
+        >
+          <router-link to="/home" :class="{'font-normal': currentRoute !== '/home', 'font-bold': currentRoute === '/home'}">
+            <i
+              class="pr-3"
+              :class="{
+                'fa-regular': currentRoute !== '/home',
+                'fa-solid': currentRoute === '/home',
+                'fa-building': true,
+              }"
+            ></i>
+
+            Home
+          </router-link>
+        </div>
+        <div
+          class="p-5 ml-5 mt-4 py-[12px] rounded-l-[5px]"
+          :class="{ 'bg-[#9EBDFF]': currentRoute === '/bewaard' }"
+        >
+          <router-link to="/bewaard" :class="{'font-normal': currentRoute !== '/bewaard', 'font-bold': currentRoute === '/bewaard'}">
+            <i
+              class="pr-3"
+              :class="{
+                'fa-regular': currentRoute !== '/bewaard',
+                'fa-solid': currentRoute === '/bewaard',
+                'fa-bookmark': true,
+              }"
+            ></i>
+            Bewaard
+          </router-link>
+        </div>
+        <div
+          class="p-5 ml-5 mt-4 py-[12px] rounded-l-[5px]"
+          :class="{ 'bg-[#9EBDFF]': currentRoute === '/account' }"
+        >
+          <router-link to="/account" :class="{'font-normal': currentRoute !== '/account', 'font-bold': currentRoute === '/account'}">
+            <i
+              class="pr-3"
+              :class="{
+                'fa-regular': currentRoute !== '/account',
+                'fa-solid': currentRoute === '/account',
+                'fa-user': true,
+              }"
+            ></i>
+            Account
+          </router-link>
+        </div>
+        <div class="">
+          <router-link to="/account" class="">
+            <img src="" alt="mascotte" />
+          </router-link>
+        </div>
+      </div>
     </div>
-    <div class="dropdown hidden">
-      <div class="w-full bg-red-300 p-5">
-        <router-link to="/home">
-          <i class="fa-solid fa-house pr-3 relative right-[2.5px]"></i>
+    <div class="dropdown bg-[#EDF0F5] hidden">
+      <div
+        class="w-full p-5"
+        :class="{ 'bg-[#9EBDFF]': currentRoute === '/home' }"
+      >
+        <router-link to="/home" :class="{'font-normal': currentRoute !== '/home', 'font-bold': currentRoute === '/home'}">
+          <i
+            class="pr-3"
+            :class="{
+              'fa-regular': currentRoute !== '/home',
+              'fa-solid': currentRoute === '/home',
+              'fa-building': true,
+            }"
+          ></i>
           Home
         </router-link>
       </div>
-      <div class="w-full bg-red-300 p-5">
-        <router-link to="/bewaard" class="">
-          <i class="fa-solid fa-bookmark pr-3"></i>
+      <div
+        class="w-full p-5"
+        :class="{ 'bg-[#9EBDFF]': currentRoute === '/bewaard' }"
+      >
+        <router-link to="/bewaard" :class="{'font-normal': currentRoute !== '/bewaard', 'font-bold': currentRoute === '/bewaard'}">
+          <i
+            class="pr-3"
+            :class="{
+              'fa-regular': currentRoute !== '/bewaard',
+              'fa-solid': currentRoute === '/bewaard',
+              'fa-bookmark': true,
+            }"
+          ></i>
           Bewaard
         </router-link>
       </div>
-      <div class="w-full bg-red-300 p-5">
-        <router-link to="/account" class="">
-          <i class="fa-solid fa-user pr-3"></i>
+      <div
+        class="w-full p-5"
+        :class="{ 'bg-[#9EBDFF]': currentRoute === '/account' }"
+      >
+        <router-link to="/account" :class="{'font-normal': currentRoute !== '/account', 'font-bold': currentRoute === '/account'}">
+          <i
+            class="pr-3"
+            :class="{
+              'fa-regular': currentRoute !== '/account',
+              'fa-solid': currentRoute === '/account',
+              'fa-user': true,
+            }"
+          ></i>
           Account
         </router-link>
       </div>
