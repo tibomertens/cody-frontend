2f270038469c6a786b85e29d4dc88af4e03138ec
TiboMertens <tibomertens25@gmail.com>
2024-03-03 14:24:17 +0100

    feat: add selected item styles

--- src/components/HomeType.vue
+++ src/components/HomeType.vue
@@ -14,13 +14,13 @@ const handleItemSelected = (itemName) => {
   <section>
     <h2>Woningkenmerken</h2>
     <div class="grid grid-cols-1 gap-[32px] xs:grid-cols-2 md:grid-cols-3">
-      <Type :item-name="'Open bebouwing'" :image-url="'../src/assets/openBebouwing.svg'"
+      <Type :item-name="'Open bebouwing'" :image-url="'../src/assets/openBebouwing.svg'" :selected="selectedItem"
         @itemSelected="handleItemSelected" />
       <Type :item-name="'Halfopen bebouwing'" :image-url="'../src/assets/halfopenBebouwing.svg'"
-        @itemSelected="handleItemSelected" />
+        :selected="selectedItem" @itemSelected="handleItemSelected" />
       <div class="xs:col-span-2 md:col-span-1">
         <Type :item-name="'Gesloten bebouwing'" :image-url="'../src/assets/geslotenBebouwing.svg'"
-          @itemSelected="handleItemSelected" />
+          :selected="selectedItem" @itemSelected="handleItemSelected" />
       </div>
     </div>
     <div class="mt-[32px]">
