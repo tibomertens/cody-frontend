9e047f66be3c130dd8fae4edff559ef1b0be0146
QuinttAdam <116001984+QuinttAdam@users.noreply.github.com>
2024-03-17 17:13:25 +0100

    fix: change setting icon

--- src/components/shared/TopNav.vue
+++ src/components/shared/TopNav.vue
@@ -22,7 +22,7 @@ function toggleActiveLink() {
       <div class="pt-[24px] flex items-center justify-between">
         <h1 class="text-title font-bold mb-2">Home</h1>
         <router-link to="#" class="flex items-center ml-2 pr-5">
-          <i class="fa-solid fa-gear"></i>
+          <img src="/settings.svg" alt="settings">
         </router-link>
       </div>
       <div class="flex gap-6 md:gap-[2rem] overflow-x-auto xl:mt-[12px] font-bold text-[1.2rem]">
