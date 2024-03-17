9b813a74b147c92039de42413124e5bf2512ed29
TiboMertens <tibomertens25@gmail.com>
2024-03-17 16:11:12 +0100

    feat: Add border color based on text value in Project.vue

--- src/components/widgets/Project.vue
+++ src/components/widgets/Project.vue
@@ -26,11 +26,16 @@ const props = defineProps({
 </script>
 
 <template>
-    <div class="w-full bg-offWhite-light mb-[40px] rounded-[6px]">
+    <div class="w-full bg-offWhite-light mb-[40px] rounded-[6px]" :class="{
+        'border-l-4 border-secondary-green': text[0] === 'Laagste impact',
+        'border-l-4 border-secondary-yellow': text[0] === 'Middelmatige impact',
+        'border-l-4 border-secondary-red': text[0] === 'Hoogste impact'
+    }">
         <div class="p-[32px]">
             <div>
                 <h2 class="font-bold text-subtitle">{{ name }}</h2>
-                <p class="mt-[12px] font-light">{{ desc }} <span class="text-primary-dark font-bold">Read more</span></p>
+                <p class="mt-[12px] font-light">{{ desc }} <span class="text-primary-dark font-bold">Read more</span>
+                </p>
             </div>
             <div class="grid grid-cols-1 xs:grid-cols-2 ml:grid-cols-3 gap-[32px] mt-[32px]">
                 <ProjectInfo :src="src[0]" :text="text[0]" :label="label[0]" />
