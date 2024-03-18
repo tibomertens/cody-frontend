aac999d0eeb52d5aa919f9583a5141365c3f37a7
QuinttAdam <116001984+QuinttAdam@users.noreply.github.com>
2024-03-18 18:56:50 +0100

    feat: add searchbar component

---
+++ src/components/UI/Searchbar.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { defineProps, defineEmits } from 'vue';
+
+const props = defineProps({
+  name: {
+    type: String,
+    required: true
+  },
+  width: {
+    type: [String, Number],
+    required: false
+  }
+});
+</script>
+
+<template>
+    <img src="/vergrootglas.svg" alt="vergrootglas" class="relative top-8 left-[14%] xl:left-[22%]">
+  <input type="text" placeholder="Zoeken..." class="text-sm h-[48px] w-[80%] ml-[10%] pl-20 rounded outline-none focus:border-2 focus:border-primary-dark xl:w-[60%] xl:ml-[20%]">
+</template>
+
+<style scoped></style>
