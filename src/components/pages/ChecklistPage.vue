fe0bae11f24283ce8c541c9b5820e6dc0f05bc1b
QuinttAdam <116001984+QuinttAdam@users.noreply.github.com>
2024-03-21 19:22:55 +0100

    feat: save selected items in reactive array

--- src/components/pages/ChecklistPage.vue
+++ src/components/pages/ChecklistPage.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, onMounted } from "vue";
+import { ref, onMounted, reactive } from "vue";
 import { useRouter } from "vue-router";
 import { isValidToken, getUser } from "../../functions/user.js";
 import ChecklistFrame from "../widgets/Checklist-frame.vue";
@@ -9,6 +9,8 @@ const router = useRouter();
 const token = localStorage.getItem("token");
 let userData = ref({});
 
+const selectedItems = reactive([]);
+
 onMounted( async () => {
   if (isValidToken(token)) {
     userData = await getUser(token);
@@ -17,6 +19,16 @@ onMounted( async () => {
     router.push("/login");
   }
 });
+
+const addSelectedItem = (selectedItem) => {
+  if (selectedItems.includes(selectedItem)) {
+    selectedItems.splice(selectedItems.indexOf(selectedItem), 1);
+  } else {
+    selectedItems.push(selectedItem);
+  }
+  console.log(selectedItems);
+}
+
 </script>
 
 <template>
@@ -24,15 +36,15 @@ onMounted( async () => {
         <h1 class="text-title font-bold text-center pt-[62px] pb-[20px] xl:text-left xl:ml-[20%]">Checklist</h1>
         <p class="text-body w-[80%] xl:w-[60%] ml-[10%] xl:ml-[20%] pb-[32px]">Duid in volgende lijst aan welke verbeterpunten op jouw EPC rapport staan.</p>
         <h2 class="text-subtitle font-bold ml-[10%] xl:ml-[20%] pb-[20px]">Duid aan</h2>
-        <ChecklistFrame title="Dak-isolatie" paragraph="Verbeter de energie-efficiëntie van je woning door te investeren in hoogwaardige dakisolatie, waardoor warmteverlies wordt geminimaliseerd." />
-        <ChecklistFrame title="Vloer-isolatie" paragraph="Verhoog het comfort en de duurzaamheid van je huis met vloerisolatie, waardoor koude vanuit de grond wordt tegengehouden en de energieprestaties verbeteren." />
-        <ChecklistFrame title="Muur-isolatie" paragraph="Optimaliseer de thermische prestaties van je woning met muurisolatie, die zorgt voor een effectieve barrière tegen warmteverlies en koude infiltratie." />
-        <ChecklistFrame title="Zonne-energie" paragraph="Maak gebruik van duurzame zonne-energie om je huis van groene stroom te voorzien, waardoor je niet alleen kosten bespaart, maar ook bijdraagt aan milieuvriendelijke energieproductie." />
-        <ChecklistFrame title="Verwarming" paragraph="Kies voor energiezuinige verwarmingsopties om het comfort in huis te handhaven terwijl je het energieverbruik minimaliseert." />
-        <ChecklistFrame title="Koeling" paragraph="Implementeer efficiënte koelsystemen om een aangename temperatuur te behouden en tegelijkertijd energie te besparen." />
-        <ChecklistFrame title="Ventilatie" paragraph="Optimaliseer de luchtkwaliteit in je woning door middel van doeltreffende ventilatiesystemen, die zorgen voor een gezond en comfortabel binnenklimaat." />
-        <ChecklistFrame title="Sanitair warm water" paragraph="Bespaar energie en kosten door te investeren in geavanceerde systemen voor sanitair warm water, die efficiëntie en duurzaamheid bevorderen." />
-        <ChecklistFrame title="Beglazing" paragraph="Verhoog de isolatiewaarde van je huis door energiezuinige beglazing te installeren, waardoor warmteverlies wordt verminderd en de energie-efficiëntie toeneemt." />
+        <ChecklistFrame @select="addSelectedItem" :title="'Dak-isolatie'" :paragraph="'Verbeter de energie-efficiëntie van je woning door te investeren in hoogwaardige dakisolatie, waardoor warmteverlies wordt geminimaliseerd.'" />
+        <ChecklistFrame @select="addSelectedItem" :title="'Vloer-isolatie'" :paragraph="'Verhoog het comfort en de duurzaamheid van je huis met vloerisolatie, waardoor koude vanuit de grond wordt tegengehouden en de energieprestaties verbeteren.'" />
+        <ChecklistFrame @select="addSelectedItem" :title="'Muur-isolatie'" :paragraph="'Optimaliseer de thermische prestaties van je woning met muurisolatie, die zorgt voor een effectieve barrière tegen warmteverlies en koude infiltratie.'" />
+        <ChecklistFrame @select="addSelectedItem" :title="'Zonne-energie'" :paragraph="'Maak gebruik van duurzame zonne-energie om je huis van groene stroom te voorzien, waardoor je niet alleen kosten bespaart, maar ook bijdraagt aan milieuvriendelijke energieproductie.'" />
+        <ChecklistFrame @select="addSelectedItem" :title="'Verwarming'" :paragraph="'Kies voor energiezuinige verwarmingsopties om het comfort in huis te handhaven terwijl je het energieverbruik minimaliseert.'" />
+        <ChecklistFrame @select="addSelectedItem" :title="'Koeling'" :paragraph="'Implementeer efficiënte koelsystemen om een aangename temperatuur te behouden en tegelijkertijd energie te besparen.'" />
+        <ChecklistFrame @select="addSelectedItem" :title="'Ventilatie'" :paragraph="'Optimaliseer de luchtkwaliteit in je woning door middel van doeltreffende ventilatiesystemen, die zorgen voor een gezond en comfortabel binnenklimaat.'" />
+        <ChecklistFrame @select="addSelectedItem" :title="'Sanitair warm water'" :paragraph="'Bespaar energie en kosten door te investeren in geavanceerde systemen voor sanitair warm water, die efficiëntie en duurzaamheid bevorderen.'" />
+        <ChecklistFrame @select="addSelectedItem" :title="'Beglazing'" :paragraph="'Verhoog de isolatiewaarde van je huis door energiezuinige beglazing te installeren, waardoor warmteverlies wordt verminderd en de energie-efficiëntie toeneemt.'" /> 
     </div>
 </template>
 
--- src/components/widgets/Checklist-frame.vue
+++ src/components/widgets/Checklist-frame.vue
@@ -5,10 +5,15 @@ const props = defineProps({
   title: String,
   paragraph: String
 });
+const emit = defineEmits(['select'])
+
+const emitSelect = () => {
+  emit('select', props.title);
+}
 </script>
 
 <template>
-  <div class="flex justify-center">
+  <div class="flex justify-center" @click="emitSelect">
     <div class="w-[80%] bg-offWhite-light rounded p-[32px] mb-[32px] xl:w-[60%]">
       <h3 class="text-[1.2rem] font-bold pb-[8px]">{{ props.title }}</h3>
       <p class="text-body">{{ props.paragraph }}</p>
