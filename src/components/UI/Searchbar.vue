16eedf7f84238d79eaef888a41854955bd9b328c
TiboMertens <tibomertens25@gmail.com>
2024-03-30 17:00:39 +0100

    Update Searchbar styling

--- src/components/UI/Searchbar.vue
+++ src/components/UI/Searchbar.vue
@@ -16,8 +16,8 @@ const props = defineProps({
 <template>
   <div class="relative">
     <input type="text" placeholder="Zoeken..."
-      class="text-sm h-[48px] w-full pl-[80px] rounded-[5px] outline-none focus:border-2 focus:border-primary-dark">
-    <img src="/vergrootglas.svg" alt="search icon" class="absolute inset-y-0 left-0 pl-[32px] pt-[16px] scale-[0.9] pointer-events-none">
+      class="text-sm h-[48px] w-full pl-[64px] rounded-[5px] outline-none focus:border-2 focus:border-primary-dark">
+    <img src="/vergrootglas.svg" alt="search icon" class="absolute inset-y-0 left-0 pl-[24px] pt-[16px] scale-[0.9] pointer-events-none">
   </div>
 </template>
 
