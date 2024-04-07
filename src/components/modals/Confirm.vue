bf725d118effe9d9c6398f238d0aef454a28934e
QuinttAdam <116001984+QuinttAdam@users.noreply.github.com>
2024-04-07 16:05:43 +0200

    style: format documents

--- src/components/modals/Confirm.vue
+++ src/components/modals/Confirm.vue
@@ -1,43 +1,46 @@
 <template>
-    <div v-if="showConfirm" class="fixed inset-0 z-50 flex items-center justify-center bg-offBlack bg-opacity-50 w-full" @click="handleOutsideClick">
-      <div class="bg-offWhite-light p-7 rounded font-bold text-btn">
-        <h2>{{ title }}</h2>
-        <div class="mt-10">
-          <div class="w-full">
-            <Btn @click="closeConfirm" name="Annuleer" />
-          </div>
-          <div class="w-full mt-4">
-            <Btn @click="confirmAction" name="Doorgaan" />
-          </div>
+  <div
+    v-if="showConfirm"
+    class="fixed inset-0 z-50 flex items-center justify-center bg-offBlack bg-opacity-50 w-full"
+    @click="handleOutsideClick"
+  >
+    <div class="bg-offWhite-light p-7 rounded font-bold text-btn">
+      <h2>{{ title }}</h2>
+      <div class="mt-10">
+        <div class="w-full">
+          <Btn @click="closeConfirm" name="Annuleer" />
+        </div>
+        <div class="w-full mt-4">
+          <Btn @click="confirmAction" name="Doorgaan" />
         </div>
       </div>
     </div>
-  </template>
-  
-  <script setup>
-  import { defineProps, defineEmits } from 'vue';
-  import Btn from '../UI/Btn.vue';
-  
-  const props = defineProps({
-    showConfirm: Boolean,
-    title: String
-  });
-  
-  const emit = defineEmits(['closeConfirm', 'confirmAction']);
-  
-  const closeConfirm = () => {
-    emit('closeConfirm');
-  };
+  </div>
+</template>
 
-  const confirmAction = () => {
-    closeConfirm(); // Close the confirm modal
-    emit('confirmAction'); // Emit event to indicate confirm action
+<script setup>
+import { defineProps, defineEmits } from "vue";
+import Btn from "../UI/Btn.vue";
+
+const props = defineProps({
+  showConfirm: Boolean,
+  title: String,
+});
+
+const emit = defineEmits(["closeConfirm", "confirmAction"]);
+
+const closeConfirm = () => {
+  emit("closeConfirm");
 };
 
-  
-  const handleOutsideClick = (event) => {
-    if (event.target === event.currentTarget) {
-      closeConfirm();
-    }
-  };
-  </script>
+const confirmAction = () => {
+  closeConfirm(); // Close the confirm modal
+  emit("confirmAction"); // Emit event to indicate confirm action
+};
+
+const handleOutsideClick = (event) => {
+  if (event.target === event.currentTarget) {
+    closeConfirm();
+  }
+};
+</script>
--- src/components/pages/ChecklistPage.vue
+++ src/components/pages/ChecklistPage.vue
@@ -12,25 +12,25 @@ import CalculatedLabelModal from "../modals/CalculatedLabel.vue";
 import Confirm from "../modals/Confirm.vue";
 
 const router = useRouter();
-const currentPath = ref('');
+const currentPath = ref("");
 
 const token = localStorage.getItem("token");
 let userData = ref({});
 let userId = ref(null);
 
 const selectedItems = reactive([
-    'Dak-isolatie', 
-    'Vloer-isolatie',
-    'Muur-isolatie', 
-    'Zonne-energie', 
-    'Verwarming', 
-    'Koeling', 
-    'Ventilatie', 
-    'Sanitair-warm-water', 
-    'Beglazing'
+  "Dak-isolatie",
+  "Vloer-isolatie",
+  "Muur-isolatie",
+  "Zonne-energie",
+  "Verwarming",
+  "Koeling",
+  "Ventilatie",
+  "Sanitair-warm-water",
+  "Beglazing",
 ]);
 
-onMounted( async () => {
+onMounted(async () => {
   if (isValidToken(token)) {
     userData = await getUser(token);
     userId = userData._id;
@@ -47,13 +47,11 @@ const addSelectedItem = (selectedItem) => {
     selectedItems.push(selectedItem);
   }
   console.log(selectedItems);
-}
+};
 
-const emit = defineEmits(['selectedSurface']);
+const emit = defineEmits(["selectedSurface"]);
 
-
-
-const selectedLabel = ref('');
+const selectedLabel = ref("");
 let labelData = ref({
   label: selectedLabel,
 });
@@ -63,60 +61,57 @@ let error = ref(null);
 
 const labelOptions = [
   {
-    title: 'Label A+',
-    name: 'A+'
+    title: "Label A+",
+    name: "A+",
   },
   {
-    title: 'Label A',
-    name: 'A'
+    title: "Label A",
+    name: "A",
   },
   {
-    title: 'Label B',
-    name: 'B'
+    title: "Label B",
+    name: "B",
   },
   {
-    title: 'Label C',
-    name: 'C'
+    title: "Label C",
+    name: "C",
   },
   {
-    title: 'Label D',
-    name: 'D'
+    title: "Label D",
+    name: "D",
   },
   {
-    title: 'Label E',
-    name: 'E'
+    title: "Label E",
+    name: "E",
   },
   {
-    title: 'Label F',
-    name: 'F'
+    title: "Label F",
+    name: "F",
   },
-]
+];
 
 const handleSelectedLabel = (itemAlias) => {
   selectedLabel.value = itemAlias;
   console.log(selectedLabel.value);
-}
-
+};
 
 const checkLabelNotEmpty = async () => {
-    if ( !selectedLabel.value) {
-      error.value = 'Please select a label';
-      return;
-    }
-    else if (selectedItems.length !== 9){
-        showModal.value = true;
-    }
-    else {
-        showConfirm.value = true;
-    }
-}
+  if (!selectedLabel.value) {
+    error.value = "Please select a label";
+    return;
+  } else if (selectedItems.length !== 9) {
+    showModal.value = true;
+  } else {
+    showConfirm.value = true;
+  }
+};
 
 const closeModal = () => {
-    showModal.value = false;
-}
+  showModal.value = false;
+};
 const closeConfirm = () => {
-    showConfirm.value = false;
-}
+  showConfirm.value = false;
+};
 
 onMounted(() => {
   currentPath.value = router.currentRoute.value.path;
@@ -131,36 +126,86 @@ onMounted(() => {
       </div>
       <h1 class="text-title font-bold text-center">Checklist</h1>
     </div>
-    <p class="text-body w-[80%] xl:w-[60%] ml-[10%] xl:ml-[20%] pb-[32px]">Duid in volgende lijst aan welke
-      verbeterpunten op jouw EPC rapport staan.</p>
-    <h2 class="text-subtitle font-bold ml-[10%] xl:ml-[20%] pb-[12px]">Huidig label</h2>
+    <p class="text-body w-[80%] xl:w-[60%] ml-[10%] xl:ml-[20%] pb-[32px]">
+      Duid in volgende lijst aan welke verbeterpunten op jouw EPC rapport staan.
+    </p>
+    <h2 class="text-subtitle font-bold ml-[10%] xl:ml-[20%] pb-[12px]">
+      Huidig label
+    </h2>
     <div class="ml-[10%] mr-[10%] xl:ml-[20%] pb-[32px]">
-      <Dropdown :items="labelOptions" :labelData="labelData" @itemSelected="handleSelectedLabel" />
+      <Dropdown
+        :items="labelOptions"
+        :labelData="labelData"
+        @itemSelected="handleSelectedLabel"
+      />
     </div>
-    <h2 class="text-subtitle font-bold ml-[10%] xl:ml-[20%] pb-[20px]">Duid aan</h2>
-    <ChecklistFrame @select="addSelectedItem" :title="'Dak-isolatie'"
-      :paragraph="'Verbeter de energie-efficiëntie van je woning door te investeren in hoogwaardige dakisolatie, waardoor warmteverlies wordt geminimaliseerd.'" />
-    <ChecklistFrame @select="addSelectedItem" :title="'Vloer-isolatie'"
-      :paragraph="'Verhoog het comfort en de duurzaamheid van je huis met vloerisolatie, waardoor koude vanuit de grond wordt tegengehouden en de energieprestaties verbeteren.'" />
-    <ChecklistFrame @select="addSelectedItem" :title="'Muur-isolatie'"
-      :paragraph="'Optimaliseer de thermische prestaties van je woning met muurisolatie, die zorgt voor een effectieve barrière tegen warmteverlies en koude infiltratie.'" />
-    <ChecklistFrame @select="addSelectedItem" :title="'Zonne-energie'"
-      :paragraph="'Maak gebruik van duurzame zonne-energie om je huis van groene stroom te voorzien, waardoor je niet alleen kosten bespaart, maar ook bijdraagt aan milieuvriendelijke energieproductie.'" />
-    <ChecklistFrame @select="addSelectedItem" :title="'Verwarming'"
-      :paragraph="'Kies voor energiezuinige verwarmingsopties om het comfort in huis te handhaven terwijl je het energieverbruik minimaliseert.'" />
-    <ChecklistFrame @select="addSelectedItem" :title="'Koeling'"
-      :paragraph="'Implementeer efficiënte koelsystemen om een aangename temperatuur te behouden en tegelijkertijd energie te besparen.'" />
-    <ChecklistFrame @select="addSelectedItem" :title="'Ventilatie'"
-      :paragraph="'Optimaliseer de luchtkwaliteit in je woning door middel van doeltreffende ventilatiesystemen, die zorgen voor een gezond en comfortabel binnenklimaat.'" />
-    <ChecklistFrame @select="addSelectedItem" :title="'Sanitair warm water'"
-      :paragraph="'Bespaar energie en kosten door te investeren in geavanceerde systemen voor sanitair warm water, die efficiëntie en duurzaamheid bevorderen.'" />
-    <ChecklistFrame @select="addSelectedItem" :title="'Beglazing'"
-      :paragraph="'Verhoog de isolatiewaarde van je huis door energiezuinige beglazing te installeren, waardoor warmteverlies wordt verminderd en de energie-efficiëntie toeneemt.'" />
+    <h2 class="text-subtitle font-bold ml-[10%] xl:ml-[20%] pb-[20px]">
+      Duid aan
+    </h2>
+    <ChecklistFrame
+      @select="addSelectedItem"
+      :title="'Dak-isolatie'"
+      :paragraph="'Verbeter de energie-efficiëntie van je woning door te investeren in hoogwaardige dakisolatie, waardoor warmteverlies wordt geminimaliseerd.'"
+    />
+    <ChecklistFrame
+      @select="addSelectedItem"
+      :title="'Vloer-isolatie'"
+      :paragraph="'Verhoog het comfort en de duurzaamheid van je huis met vloerisolatie, waardoor koude vanuit de grond wordt tegengehouden en de energieprestaties verbeteren.'"
+    />
+    <ChecklistFrame
+      @select="addSelectedItem"
+      :title="'Muur-isolatie'"
+      :paragraph="'Optimaliseer de thermische prestaties van je woning met muurisolatie, die zorgt voor een effectieve barrière tegen warmteverlies en koude infiltratie.'"
+    />
+    <ChecklistFrame
+      @select="addSelectedItem"
+      :title="'Zonne-energie'"
+      :paragraph="'Maak gebruik van duurzame zonne-energie om je huis van groene stroom te voorzien, waardoor je niet alleen kosten bespaart, maar ook bijdraagt aan milieuvriendelijke energieproductie.'"
+    />
+    <ChecklistFrame
+      @select="addSelectedItem"
+      :title="'Verwarming'"
+      :paragraph="'Kies voor energiezuinige verwarmingsopties om het comfort in huis te handhaven terwijl je het energieverbruik minimaliseert.'"
+    />
+    <ChecklistFrame
+      @select="addSelectedItem"
+      :title="'Koeling'"
+      :paragraph="'Implementeer efficiënte koelsystemen om een aangename temperatuur te behouden en tegelijkertijd energie te besparen.'"
+    />
+    <ChecklistFrame
+      @select="addSelectedItem"
+      :title="'Ventilatie'"
+      :paragraph="'Optimaliseer de luchtkwaliteit in je woning door middel van doeltreffende ventilatiesystemen, die zorgen voor een gezond en comfortabel binnenklimaat.'"
+    />
+    <ChecklistFrame
+      @select="addSelectedItem"
+      :title="'Sanitair warm water'"
+      :paragraph="'Bespaar energie en kosten door te investeren in geavanceerde systemen voor sanitair warm water, die efficiëntie en duurzaamheid bevorderen.'"
+    />
+    <ChecklistFrame
+      @select="addSelectedItem"
+      :title="'Beglazing'"
+      :paragraph="'Verhoog de isolatiewaarde van je huis door energiezuinige beglazing te installeren, waardoor warmteverlies wordt verminderd en de energie-efficiëntie toeneemt.'"
+    />
     <div class="flex justify-center pb-[32px]">
       <Btn :name="'Doorgaan'" @click="checkLabelNotEmpty()" :width="''" />
     </div>
-    <CalculatedLabelModal :showModal="showModal" :labelData="labelData" :items="selectedItems" :userId="userId" @closeModal="closeModal" :path="currentPath"/>
-    <div v-if="error" class="text-red-500 text-center pb-[32px]">{{ error }}</div>
-    <Confirm :showConfirm="showConfirm" :title="'Weet je zeker dat je niets wilt aanduiden?'" @closeConfirm="closeConfirm" @confirmAction="showModal = true" />
+    <CalculatedLabelModal
+      :showModal="showModal"
+      :labelData="labelData"
+      :items="selectedItems"
+      :userId="userId"
+      @closeModal="closeModal"
+      :path="currentPath"
+    />
+    <div v-if="error" class="text-red-500 text-center pb-[32px]">
+      {{ error }}
+    </div>
+    <Confirm
+      :showConfirm="showConfirm"
+      :title="'Weet je zeker dat je niets wilt aanduiden?'"
+      @closeConfirm="closeConfirm"
+      @confirmAction="showModal = true"
+    />
   </div>
 </template>
