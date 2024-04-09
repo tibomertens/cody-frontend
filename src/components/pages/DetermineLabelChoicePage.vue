18118b8e45bb1a2b3c4546ab39503ad193740b91
TiboMertens <tibomertens25@gmail.com>
2024-04-09 10:08:10 +0200

    fix: title font size

--- src/components/pages/DetermineLabelChoicePage.vue
+++ src/components/pages/DetermineLabelChoicePage.vue
@@ -9,7 +9,7 @@ const route = useRoute();
 <template>
   <div class="p-10 h-screen content-center lg:flex">
     <div class="lg:w-1/2 lg:content-center">
-      <h2 class="text-subtitle font-bold text-center pb-3">Ik heb een EPC-rapport</h2>
+      <h2 class="text-title font-bold text-center pb-3">Ik heb een EPC-rapport</h2>
       <p class="text-body text-center pb-5 lg:w-[80%] ml-[10%]">Er is al eens een expert bij jou thuis langsgeweest en die heeft een EPC-rapport voor jou opgeesteld en je hebt hiervan een EPC-score & label gekregen. Als je deze bij de hand hebt kun je deze gebruiken om de benodigde info in te vullen.</p>
       <div class="hidden w-full lg:flex justify-center my-5">
         <img class="w-[360px] h-[360px] border border-red-500" src="#" alt="Rapport">
@@ -20,7 +20,7 @@ const route = useRoute();
     </div>
     <div class="w-full border-b-4 border-offBlack opacity-10 my-10 lg:border-s-4 lg:border-b-0 lg:w-1 lg:mx-10"></div>
     <div class="lg:w-1/2 lg:content-center">
-      <h2 class="text-subtitle font-bold text-center pb-3">Ik heb geen EPC-rapport</h2>
+      <h2 class="text-title font-bold text-center pb-3">Ik heb geen EPC-rapport</h2>
       <p class="text-body text-center pb-5 lg:w-[80%] ml-[10%]">Je hebt nog geen idee wat je score of label is en je hebt nog geen professionele hulp gehad maar wilt wel al aan de slag of eens kijken wat je te wachten staat. </p>
       <div class="hidden lg:flex justify-center my-5">
         <img class="w-[360px] h-[360px]  border border-red-500" src="#" alt="Indicatieve test">
