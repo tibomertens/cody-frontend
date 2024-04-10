e3f9c7d2b48cd8c7f27b58edbe92309998283b0c
TiboMertens <tibomertens25@gmail.com>
2024-04-10 16:08:35 +0200

    fix: remove border

--- src/components/UI/Input.vue
+++ src/components/UI/Input.vue
@@ -46,7 +46,7 @@ watch(
     </div>
     <div class="relative">
       <input :type="type"
-        :class="{ 'border-2 border-secondary-red': hasError, 'border-2': !hasError, 'pl-[48px]': props.preFix }"
+        :class="{ 'border-2 border-secondary-red': hasError, 'border-2 border-offWhite-light': !hasError, 'pl-[48px]': props.preFix }"
         class="w-[100%] p-2 rounded-md focus:border-primary-dark focus:outline-none pl-[24px]" v-model="inputValue"
         @input="updateInput" :placeholder="props.placeholder" />
       <p v-if="props.preFix" class="font-bold absolute inset-y-0 left-0 pl-[24px] pt-[9.5px] pointer-events-none">{{
