258b5c9184cb3354b2961fb4fd027c7688ecb44e
brittadam <britt.adam@hotmail.com>
2024-04-12 14:52:39 +0200

    Feat: Add Promotor-card component to PromotorsPage.vue

--- src/components/pages/PromotorsPage.vue
+++ src/components/pages/PromotorsPage.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { useRoute } from "vue-router";
 import { ref, onMounted } from "vue";
+import PromotorCard from "../widgets/Promotor-card.vue";
 
 // put all information of the promotors in object using a get all promotors api call
 const promotors = ref([]);
@@ -27,21 +28,8 @@ onMounted(() => {
     <div class="">
         <ul class="">
             <li v-for="promotor in promotors" class="sm:flex sm:flex-wrap lg:justify-between bg-offWhite-light my-[32px] mx-[40px] py-[12px] px-[32px] rounded-md">
-                <div class="flex justify-center pt-[10px] font-bold sm:w-[50%] lg:w-[15%]">
-                    {{ promotor.name }} 
-                </div>
-                <div class="flex justify-center pt-[10px] sm:w-[50%] lg:w-[15%] lg:text-center">
-                    {{ promotor.location }}
-                </div>
-                <div class="flex justify-center pt-[10px] sm:w-[50%] lg:w-[15%] lg:text-center">
-                    {{ promotor.phoneNumber }}
-                </div>
-                <div class="flex justify-center pt-[10px] pb-[10px] items-center lg:justify-end gap-[10px] sm:w-[50%] lg:w-[15%] lg:text-right">
-                  <i class="fa-solid fa-star" style="color: #ff9900;"></i>
-                  <p>5/5</p>
-                  <a href="#" class="underline">Reviews</a>
-                </div>
-            </li>
+               <Promotor-card :promotor="promotor"/>
+            </li> 
         </ul>
     </div>
 </template>
---
+++ src/components/widgets/Promotor-card.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+import { ref, defineProps } from "vue";
+
+const props = defineProps(["promotor"]);
+</script>
+
+<template>
+    <div class="flex justify-center pt-[10px] font-bold sm:w-[50%] lg:w-[15%]">
+        {{ promotor.name }}
+    </div>
+    <div class="flex justify-center pt-[10px] sm:w-[50%] lg:w-[15%] lg:text-center">
+        {{ promotor.location }}
+    </div>
+    <div class="flex justify-center pt-[10px] sm:w-[50%] lg:w-[15%] lg:text-center">
+        {{ promotor.phoneNumber }}
+    </div>
+    <div
+        class="flex justify-center pt-[10px] pb-[10px] items-center lg:justify-end gap-[10px] sm:w-[50%] lg:w-[15%] lg:text-right">
+        <i class="fa-solid fa-star" style="color: #ff9900;"></i>
+        <p>5/5</p>
+        <a href="#" class="underline">Reviews</a>
+    </div>
+</template>
+
+<style scoped></style>
