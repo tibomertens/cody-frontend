ddbc2b1b59942a564cb6f42c69a5f6f68734e369
brittadam <britt.adam@hotmail.com>
2024-04-12 14:38:05 +0200

    Feat: add review to promotors

--- src/components/pages/PromotorsPage.vue
+++ src/components/pages/PromotorsPage.vue
@@ -24,15 +24,26 @@ onMounted(() => {
 </script>
 
 <template>
-<ul>
-    <li v-for="promotor in promotors">
-        {{ promotor.name }} 
-        {{ promotor.location }}
-        {{ promotor.phoneNumber }}
-        
-    </li>
-</ul>
-
+    <div class="">
+        <ul class="">
+            <li v-for="promotor in promotors" class="sm:flex sm:flex-wrap lg:justify-between bg-offWhite-light my-[32px] mx-[40px] py-[12px] px-[32px] rounded-md">
+                <div class="flex justify-center pt-[10px] font-bold sm:w-[50%] lg:w-[15%]">
+                    {{ promotor.name }} 
+                </div>
+                <div class="flex justify-center pt-[10px] sm:w-[50%] lg:w-[15%] lg:text-center">
+                    {{ promotor.location }}
+                </div>
+                <div class="flex justify-center pt-[10px] sm:w-[50%] lg:w-[15%] lg:text-center">
+                    {{ promotor.phoneNumber }}
+                </div>
+                <div class="flex justify-center pt-[10px] pb-[10px] items-center lg:justify-end gap-[10px] sm:w-[50%] lg:w-[15%] lg:text-right">
+                  <i class="fa-solid fa-star" style="color: #ff9900;"></i>
+                  <p>5/5</p>
+                  <a href="#" class="underline">Reviews</a>
+                </div>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <style scoped>
