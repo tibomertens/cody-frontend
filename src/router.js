79e80377d7663d98465858e730a76d1fbf6acc89
TiboMertens <tibomertens25@gmail.com>
2024-04-13 16:06:25 +0200

    fix: display correct pageName

--- src/router.js
+++ src/router.js
@@ -11,61 +11,79 @@ import checklist from "./components/pages/ChecklistPage.vue";
 import Settings from "./components/pages/SettingsPage.vue";
 import DetermineLabelChoicePage from "./components/pages/DetermineLabelChoicePage.vue";
 
-
 const routes = [
   {
     path: "/",
     components: { default: home, sidebar: sideNav, topNav: topNav },
-    meta: { sidebar: true },
-  },
-  {
-    path: "/saved",
-    components: { default: bewaard, sidebar: sideNav, topNav: topNav },
+    name: "Dashboard",
     meta: { sidebar: true },
   },
   {
     path: "/account",
     components: { default: account, sidebar: sideNav, topNav: topNav },
+    name: "Account",
     meta: { sidebar: true },
   },
   { path: "/login", components: { default: login }, meta: { sidebar: false } },
-  { path: "/determinelabelchoice", components: { default: DetermineLabelChoicePage }, meta: { sidebar: false } },
-  { path: "/test/berekenIndicatief", components: { default: berekenIndicatief }, meta: { sidebar: false } },
-  { path: "/rapport/berekenIndicatief", components: { default: berekenIndicatief }, meta: { sidebar: false } },
-  { path: "/rapport/checklist", components: { default: checklist }, meta: { sidebar: false } },
+  {
+    path: "/determinelabelchoice",
+    components: { default: DetermineLabelChoicePage },
+    meta: { sidebar: false },
+  },
+  {
+    path: "/test/berekenIndicatief",
+    components: { default: berekenIndicatief },
+    meta: { sidebar: false },
+  },
+  {
+    path: "/rapport/berekenIndicatief",
+    components: { default: berekenIndicatief },
+    meta: { sidebar: false },
+  },
+  {
+    path: "/rapport/checklist",
+    components: { default: checklist },
+    meta: { sidebar: false },
+  },
   {
     path: "/projects",
     components: { default: Projects, sidebar: sideNav, topNav: topNav },
+    name: "Projecten",
     meta: { sidebar: true },
   },
   {
     path: "/projects/recommended",
     components: { default: Projects, sidebar: sideNav, topNav: topNav },
+    name: "Aanbevolen projecten",
     meta: { sidebar: true },
   },
   {
     path: "/projects/active",
     components: { default: Projects, sidebar: sideNav, topNav: topNav },
+    name: "Actieve projecten",
     meta: { sidebar: true },
   },
   {
     path: "/projects/completed",
     components: { default: Projects, sidebar: sideNav, topNav: topNav },
+    name: "Afgeronde projecten",
     meta: { sidebar: true },
   },
   {
     path: "/projects/saved",
     components: { default: Projects, sidebar: sideNav, topNav: topNav },
+    name: "Opgeslagen projecten",
     meta: { sidebar: true },
   },
   {
     path: "/settings",
-    components: { default: Settings, sidebar:sideNav, topNav: topNav },
+    components: { default: Settings, sidebar: sideNav, topNav: topNav },
+    name: "Instellingen",
     meta: { sidebar: true },
   },
   {
-    path: '/:catchAll(.*)',
-    redirect: '/', 
+    path: "/:catchAll(.*)",
+    redirect: "/",
   },
 ];
 
