86948f51507a8f0c22995d53764170fca12bc213
TiboMertens <tibomertens25@gmail.com>
2024-04-14 22:50:09 +0200

    fix: accept correct prop type

--- src/components/widgets/Project.vue
+++ src/components/widgets/Project.vue
@@ -23,7 +23,7 @@ const props = defineProps({
         required: true
     },
     suggestion: {
-        type: String,
+        type: Boolean,
         required: false,
         default: false
     }
