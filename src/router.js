e9de76b850fff120416ff3cee24ff408baada62b
TiboMertens <tibomertens25@gmail.com>
2024-04-17 12:30:36 +0200

    fix: router import

--- src/router.js
+++ src/router.js
@@ -8,7 +8,7 @@ import sideNav from "./components/shared/SideNav.vue";
 import topNav from "./components/shared/TopNav.vue";
 import Projects from "./components/pages/ProjectsPage.vue";
 import checklist from "./components/pages/ChecklistPage.vue";
-import settings from "./components/pages/SettingsPage.vue";
+import Settings from "./components/pages/SettingsPage.vue";
 import promotors from "./components/pages/PromotorsPage.vue";
 import DetermineLabelChoicePage from "./components/pages/DetermineLabelChoicePage.vue";
 import RenovationDetailPage from "./components/pages/RenovationDetailsPage.vue";
