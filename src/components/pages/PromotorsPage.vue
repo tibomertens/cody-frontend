be40fbb8b3499334ab6404bcfd6f864901bc240d
TiboMertens <tibomertens25@gmail.com>
2024-04-17 12:51:57 +0200

    feat: add searchbar & delete filter

--- src/components/pages/PromotorsPage.vue
+++ src/components/pages/PromotorsPage.vue
@@ -1,8 +1,11 @@
 <script setup>
 import { ref, onMounted } from "vue";
+
 import PromotorCard from "../widgets/Promotor-card.vue";
 import PromotorCardPremium from "../widgets/Promotor-card-premium.vue";
 import Dropdown from "../UI/Dropdown.vue";
+import Searchbar from "../UI/Searchbar.vue";
+
 import { getAllPromotors, getAllLocations } from "../../functions/promotor";
 
 const promotors = ref([]);
@@ -22,10 +25,10 @@ const populateLocations = (data) => {
 
 };
 
-onMounted( async() => {
+onMounted(async () => {
   let fetchedLocations = await getAllLocations();
   populateLocations(fetchedLocations);
-  promotors.value= await getAllPromotors();
+  promotors.value = await getAllPromotors();
   filterPromotors();
 });
 
@@ -33,7 +36,7 @@ onMounted( async() => {
 const filteredPromotors = ref([]);
 
 const filterPromotors = () => {
-  if (!selectedLocation.value) {
+  if (!selectedLocation.value || selectedLocation.value === 'all') {
     filteredPromotors.value = promotors.value;
   } else {
     const topPromotors = promotors.value.filter(promotor => promotor.location === selectedLocation.value && promotor.is_top);
@@ -45,22 +48,28 @@ const filterPromotors = () => {
 </script>
 
 <template>
-  <div class="flex justify-end my-[32px] mx-[40px]"> 
-    <Dropdown :items="locations" @itemSelected="handleSelectedLocation" />
-  </div>
-  <div>
-    <ul>
-      <li v-for="promotor in filteredPromotors" :key="promotor.id" class="sm:flex sm:flex-wrap lg:justify-between bg-offWhite-light my-[32px] mx-[40px] py-[12px] px-[32px] rounded-md">
-        <template v-if="!promotor.is_big">
-          <PromotorCard :promotor="promotor"/>
-        </template>
-        <template v-else>
-          <PromotorCardPremium :promotor="promotor"/>
-        </template>
-      </li> 
-    </ul>
-  </div>
+  <section class="m-[32px] md:m-[40px]">
+    <div class="mb-[32px] md:mb-[40px] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[32px] md:gap-[40px]">
+      <Searchbar class="lg:col-span-2 mt-[36px]" />
+      <div>
+        <Dropdown :items="locations" @itemSelected="handleSelectedLocation" :default="'kies je gemeente'" :label="'all'"
+          :displayLabel="false" :width="'full'" />
+      </div>
+    </div>
+    <div>
+      <ul>
+        <li v-for="promotor in filteredPromotors" :key="promotor.id"
+          class="sm:flex sm:flex-wrap lg:justify-between bg-offWhite-light my-[32px] py-[12px] px-[32px] rounded-md">
+          <template v-if="!promotor.is_big">
+            <PromotorCard :promotor="promotor" />
+          </template>
+          <template v-else>
+            <PromotorCardPremium :promotor="promotor" />
+          </template>
+        </li>
+      </ul>
+    </div>
+  </section>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
