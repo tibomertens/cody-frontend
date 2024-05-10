2adbdcba2bb415dd774b81ab622b3ad5aec1ca3d
TiboMertens <tibomertens25@gmail.com>
2024-05-10 15:25:25 +0200

    feat: searchbar has search functionality

--- src/components/UI/Searchbar.vue
+++ src/components/UI/Searchbar.vue
@@ -1,9 +1,26 @@
 <script setup>
+import { ref } from 'vue'
+
+const props = defineProps({
+  placeholder: {
+    type: String,
+    default: 'Zoeken...'
+  }
+});
+
+const emit = defineEmits(['search'])
+
+const q = ref('');
+
+const search = () => {
+  emit('search', q.value)
+}
+
 </script>
 
 <template>
   <div class="searchbar relative">
-    <input type="text" placeholder="Zoeken..."
+    <input type="text" :placeholder="props.placeholder" aria-label="Search" v-model="q" @input="search"
       class="text-sm h-[48px] w-full pl-[64px] rounded-[5px] outline-none focus:border-2 focus:border-primary-dark">
     <img src="/vergrootglas.svg" alt="search icon"
       class="searchbar-icon absolute inset-y-0 left-0 pl-[24px] pt-[16px] scale-[0.9] pointer-events-none">
