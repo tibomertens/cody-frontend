13a5619a53bdb8eeea80c3ffbba8d636879e9316
TiboMertens <tibomertens25@gmail.com>
2024-05-18 15:59:34 +0200

    fix: padding

--- src/components/widgets/Error_state.vue
+++ src/components/widgets/Error_state.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex justify-center items-center flex-col pt-[100px] lg:pt-[150px]">
+    <div class="flex justify-center items-center flex-col pt-[50px]">
         <img src="/error_state.svg" alt="error state" class="relative right-[42px] lg:right-[0px]">
         <p class="text-btn font-bold text-secondary-red relative lg:left-[42px] pt-[12px]">{{ props.text }}</p>
     </div>
