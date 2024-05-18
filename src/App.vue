b0a2a416e321b9a6d75287fb72f3e6f403783c03
TiboMertens <tibomertens25@gmail.com>
2024-05-18 15:53:07 +0200

    fix typo

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@ const routeHasSidebar = (route) => {
   <main class="w-full bg-offWhite-dark">
     <div :class="{ 'xl:flex xl:pl-[20%]': routeHasSidebar($route) }">
       <router-view name="sidebar"></router-view>
-      <div class="xl:flex-1 min-h-screen" :class="{ 'max-w-[80vw]': routeHasSidebar($route) }">
+      <div class="xl:flex-1 min-h-screen" :class="{ 'xl:max-w-[80vw]': routeHasSidebar($route) }">
         <router-view name="topNav" class="xl:pt-[0px] pt-[75px] xl:w-[80%]"></router-view>
         <div :class="{ 'xl:pt-[86px]': routeHasSidebar($route) }">
           <router-view></router-view>
