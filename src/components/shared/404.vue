3afea402abd8cadb7d44bf571fb7408ddeb6f16a
TiboMertens <tibomertens25@gmail.com>
2024-05-31 16:23:35 +0200

    feat: add 404 redirect page

---
+++ src/components/shared/404.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="flex gap-[48px] justify-center h-calc-100vh-176 xl:h-calc-100vh-71 items-center m-[32px] sm:m-[40px]">
+        <div class="hidden sm:block">
+            <img src="/404.svg" alt="404" />
+        </div>
+        <div class="sm:relative top-[12px]">
+            <h1 class="font-bold text-[7em] text-offBlack mb-0">404</h1>
+            <p class="max-w-[400px]">Deze pagina bestaat niet. Ga terug naar het dashboard of gebruik een andere url.</p>
+            <Btn text="Ga naar dashboard" class="mt-[26px]" @click="navigateToDashboard" />
+        </div>
+    </div>
+</template>
+
+<script setup>
+import Btn from '../UI/Btn.vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const navigateToDashboard = () => {
+    router.push('/');
+}
+</script>
