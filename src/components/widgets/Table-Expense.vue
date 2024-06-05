1ddb443aaa45ebe03e2d91786cb37d20998ba098
brittadam <britt.adam@hotmail.com>
2024-06-05 11:13:30 +0200

    Feat: Add change budget to Expenses

--- src/components/widgets/Table-Expense.vue
+++ src/components/widgets/Table-Expense.vue
@@ -5,6 +5,12 @@ import { getUser, isValidToken } from '../../functions/user';
 import { useRouter } from 'vue-router';
 import { convertDate } from "../../functions/helpers.js";
 import { formatFinancialNumber } from "../../functions/helpers.js";
+import ChangeFinalBudget from "../modals/ChangeFinalBudget.vue";
+
+let showFinalBudgetModal = ref(false);
+let currentRenovation = ref(null);
+let currentUserId = ref('');
+
 
 const columns = [
   '', // Add an empty string for the new column
@@ -33,13 +39,34 @@ onMounted(async () => {
       console.log(user);
       console.log(userId.value);
 
-      renovations.value = await getAllUserRenovations(userId.value);
+     await getData();
       dataIsLoaded.value = true;
     } catch (error) {
       console.error('Failed to fetch renovations:', error);
     }
   }
 });
+
+const getData = async () => {
+  try {
+    const result = await getAllUserRenovations(userId.value);
+    console.log(result);
+    renovations.value = result;
+  } catch (error) {
+    console.error('Failed to fetch renovations:', error);
+  }
+};
+
+const openEditFinalBudgetPopup = (renovation, userId) => {
+  currentRenovation.value = renovation;
+  currentUserId.value = userId;
+  showFinalBudgetModal.value = true;
+};
+
+const closeFinalBudgetModal = () => {
+  showFinalBudgetModal.value = false;
+};
+
 </script>
 
 <template>
@@ -57,22 +84,32 @@ onMounted(async () => {
         <div v-for="(renovation, index) in renovations" :key="index">
           <div v-if="renovation.status !== 'Aanbevolen' && renovation.status !== 'extra'" class="flex justify-between items-center py-2 rounded w-[1118px]">
             <div class="w-[20px]">
-              <div ><img src="/edit_no_fill.svg" alt="potlood" /></div>
+              <a href="#" @click="openEditFinalBudgetPopup(renovation, userId.value)">
+                <div ><img src="/edit_no_fill.svg" alt="potlood" /></div>
+              </a>
             </div>
             <div class="grid grid-cols-7 bg-offWhite-light p-4 rounded w-[1084px] justify-between">
-              <div class="col-span-1">{{ formatFinancialNumber(renovation.budget) }}</div>
-              <div class="col-span-1">{{ renovation.budget_final ? formatFinancialNumber(renovation.budget_final) : '-' }}</div>
-              <div class="col-span-2 text-primary-dark font-bold" style="margin-right: 24px;">{{ renovation.renovation_title }}</div>
-              <div class="col-span-1"><i class="fa-solid fa-circle " :class="{'text-primary-dark':renovation.status === 'Voltooid' , 'text-secondary-green':renovation.status === 'Actief' , 'text-secondary-yellow':renovation.status === 'Gepauzeerd' }"></i> {{ renovation.status }} </div>
-              <div class="col-span-1">{{ convertDate(renovation.startDate) }}</div>
-              <div class="col-span-1">{{ renovation.endDate ? convertDate(renovation.endDate) : '-' }}</div>
+              <div class="col-span-1 truncate">{{ formatFinancialNumber(renovation.budget) }}</div>
+              <div class="col-span-1 truncate">{{ renovation.budget_final ? formatFinancialNumber(renovation.budget_final) : '-' }}</div>
+              <div class="col-span-2 text-primary-dark font-bold truncate" style="margin-right: 24px;">{{ renovation.renovation_title }}</div>
+              <div class="col-span-1 truncate"><i class="fa-solid fa-circle " :class="{'text-primary-dark':renovation.status === 'Voltooid' , 'text-secondary-green':renovation.status === 'Actief' , 'text-secondary-yellow':renovation.status === 'Gepauzeerd' }"></i> {{ renovation.status }} </div>
+              <div class="col-span-1 truncate">{{ convertDate(renovation.startDate) }}</div>
+              <div class="col-span-1 truncate">{{ renovation.endDate ? convertDate(renovation.endDate) : '-' }}</div>
             </div>
           </div>
         </div>
       </div>
     </div>
   </div>
+  <ChangeFinalBudget
+    :showFinalBudgetModal="showFinalBudgetModal"
+    :userId="userId"
+    @closeFinalBudgetModal="closeFinalBudgetModal"
+    @confirmAction="getData"
+    :renovation="currentRenovation"
+  />
 </template>
+
 
 
 
