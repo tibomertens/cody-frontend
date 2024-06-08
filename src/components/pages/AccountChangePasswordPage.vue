275ec06643da612f04602f60a222efecb312b926
TiboMertens <tibomertens25@gmail.com>
2024-06-08 14:59:30 +0200

    fix: positioning & loading animation

--- src/components/pages/AccountChangePasswordPage.vue
+++ src/components/pages/AccountChangePasswordPage.vue
@@ -13,6 +13,7 @@ let newPassword = ref("");
 let confirmPassword = ref("");
 let hasError = ref(false);
 let errorMessage = ref("");
+let loadingBtn = ref(false);
 
 onMounted(async () => {
   if (!isValidToken(token)) {
@@ -46,17 +47,19 @@ const handleClick = async () => {
     errorMessage.value = "Nieuw wachtwoord en bevestig wachtwoord komen niet overeen.";
     return;
   }
+  loadingBtn.value = true;
 
   // Prepare the body for the update request
   let body = {
     old_password: oldPassword.value,
-    password: newPassword.value, 
+    password: newPassword.value,
   };
 
   // Send the update request to the backend
   let result;
   try {
     result = await updateUser(token, body);
+    loadingBtn.value = false;
 
   } catch (error) {
     console.error(error);
@@ -77,27 +80,29 @@ const handleClick = async () => {
 </script>
 
 <template>
-    <div class="flex justify-center my-[10%]">
-      <div class="w-[60%] xs:w-[400px]">
-        <div class="flex items-center">
-          <h2 class="text-title font-bold">Wachtwoord veranderen</h2>
-        </div>
-        <div class="w-full xs:w-[400px]">
-          <Input label="Oud wachtwoord" type="password" @input-change="updateOldPassword" :error="hasError" :value="oldPassword" :forget="true"/>
-        </div>
-        <div class="w-full xs:w-[400px]">
-          <Input label="Nieuw wachtwoord" type="password" @input-change="updateNewPassword" :error="hasError" :value="newPassword" :forget="true"/>
-        </div>
-        <div class="w-full xs:w-[400px]">
-          <Input label="Bevestig wachtwoord" type="password" @input-change="updateConfirmPassword" :error="hasError" :value="confirmPassword" :forget="true"/>
-        </div>
-        <div v-if="hasError" class="text-secondary-red mt-2">
-          {{ errorMessage }}
-        </div>
-        <Btn class="mt-8" name="Wachtwoord veranderen" @click="handleClick" />
+  <div class="flex justify-center items-center m-[32px] lg:m-[40px] h-calc-100vh-197 xl:h-calc-100vh-151">
+    <div class="">
+      <div class="flex items-center">
+        <h2 class="text-title font-bold">Wachtwoord veranderen</h2>
       </div>
+      <div class="w-full xs:w-[400px]">
+        <Input label="Oud wachtwoord" type="password" @input-change="updateOldPassword" :error="hasError"
+          :value="oldPassword" :forget="true" />
+      </div>
+      <div class="w-full xs:w-[400px]">
+        <Input label="Nieuw wachtwoord" type="password" @input-change="updateNewPassword" :error="hasError"
+          :value="newPassword" :forget="true" />
+      </div>
+      <div class="w-full xs:w-[400px]">
+        <Input label="Bevestig wachtwoord" type="password" @input-change="updateConfirmPassword" :error="hasError"
+          :value="confirmPassword" :forget="true" />
+      </div>
+      <div v-if="hasError" class="text-secondary-red mt-2">
+        {{ errorMessage }}
+      </div>
+      <Btn class="mt-8" name="Wachtwoord veranderen" @click="handleClick" :loading="loadingBtn" />
     </div>
-  </template>
-  
-  <style scoped></style>
-  
+  </div>
+</template>
+
+<style scoped></style>
