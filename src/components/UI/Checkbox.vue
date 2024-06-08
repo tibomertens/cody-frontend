5b33f0c0981629431cf388aa4812cb49693131e0
brittadam <britt.adam@hotmail.com>
2024-06-08 18:30:53 +0200

    Fix: Update Checkbox component styling for better accessibility

--- src/components/UI/Checkbox.vue
+++ src/components/UI/Checkbox.vue
@@ -17,7 +17,7 @@ const emit = defineEmits(['selectedItem']);
   <!-- Checkbox -->
   <div class="flex gap-[6px] items-center">
     <input :id="props.label" type="checkbox"
-      class="mr-[8px] rounded-[5px] w-[20px] h-[20px] border-2 border-primary-dark outline-none appearance-none bg-offWhite-light checked:bg-primary-dark"
+      class="mr-[8px] rounded-[5px] min-w-[20px] min-h-[20px] border-2 border-primary-dark outline-none appearance-none bg-offWhite-light checked:bg-primary-dark"
       @change="emit('selectedItem', props.label)" v-model="props.isChecked" />
     <label class="relative bottom-[1px]" :for="props.label">{{ props.label }}</label>
   </div>
